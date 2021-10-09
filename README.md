f9aa21062ef182d8430724634506ccfadc150b16
David Ginanni <david.ginanni@gmail.com>
2021-10-09 16:33:14 +0200

    aggiunto script base per aggiornamento

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 /bin/wp-cli.phar --allow-root theme update --all
 
 /bin/wp-cli.phar --allow-root core update-db
+
+sudo chown -R ${MAIN_USER}:${MAIN_GROUP}
+
+## Idea 1
+
+Creare un immagine go, che lancia uno script che va a prendere le immagini elencate in una variabile settata al run dell'immagine.
+
+Da questo elenco vado a prendere l'immagine, la faccio partire, lancio i 3/4 script e via.
---
+++ updateall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for d in projects/*/
+do
+    FOLDER_NAME="$(basename "$d")"
+    echo "Processing: ${FOLDER_NAME}"
+    docker-compose up -d $FOLDER_NAME
+
+    docker-compose exec $FOLDER_NAME /bin/wp-cli.phar --allow-root core update
+    docker-compose exec $FOLDER_NAME /bin/wp-cli.phar --allow-root core update-db
+    docker-compose exec $FOLDER_NAME /bin/wp-cli.phar --allow-root plugin update --all
+    docker-compose exec $FOLDER_NAME /bin/wp-cli.phar --allow-root theme update --all
+
+    docker-compose stop $FOLDER_NAME
+done
