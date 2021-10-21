8a6c2f87ceac8679e5a16fa3adab9528f51e4ada
David Ginanni <david.ginanni@corilla.it>
2021-10-21 11:39:07 +0200

    added base installation process

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 projectsToInstall=
-initializeGit=
+useGit=
 
-while getopts 'p:u:t:n:p:e:g' arg
+while getopts 'p:u:t:n:w:e:g' arg
 do
   case $arg in
     p)  # The project to install
@@ -18,14 +18,14 @@ do
     n)  # The admin name
         adminName=${OPTARG}
         ;;
-    p)  # The admin password
+    w)  # The admin password
         adminPass=${OPTARG}
         ;;
     e)  # The admin email
         adminEmail=${OPTARG}
         ;;
     g)  # Initialize git
-        initializeGit=${OPTARG}
+        useGit=${OPTARG}
         ;;
   esac
 done
@@ -35,8 +35,9 @@ do
     FOLDER_NAME="$(basename "$d")"
     echo "Installing: ${FOLDER_NAME}"
 
-    if [ "$initializeGit" = true ]; then
+    if [ "$useGit" = true ]; then
         cd $d
+        echo "git init"
         git init
         cd ../..
     fi
@@ -48,9 +49,55 @@ do
         --admin_password=$adminPass \
         --admin_email=$adminEmail
 
-    if [ "$initializeGit" = true ]; then
+    if [ "$useGit" = true ]; then
         cd $d
+        echo "Committing the installation"
         git commit -m "First commit"
         cd ../..
     fi
+
+    echo ""
+    echo -n "Starting theme name: "
+    read -r baseThemeName
+
+    # Install the base theme
+    docker exec $FOLDER_NAME /bin/wp-cli.phar --allow-root theme install $baseThemeName
+
+    if [ "$useGit" = true ]; then
+        cd $d
+        echo "Committing theme install"
+        git add .
+        git commit -m "Added base theme: $baseThemeName"
+        cd ../..
+    fi
+
+    echo ""
+    echo "Insert the name of the plugin to install(leave blank to skip this process)"
+    echo "Most useful plugins are: wp-optimize wordpress-seo better-wp-security elementor pro-elements webp-converter-for-media"
+    pluginToInstall='no'
+    while [ ! -z "$pluginToInstall" ]
+    do
+        echo -n "Name of the plugin to install: "
+        read -r pluginToInstall
+
+        if [ -z "$pluginToInstall" ]
+        then
+            break;
+        fi
+
+        docker exec $FOLDER_NAME /bin/wp-cli.phar --allow-root plugin install $pluginToInstall
+    done
+
+    if [ "$useGit" = true ]; then
+        cd $d
+        echo "Committing plugin install"
+        git add .
+        git commit -m "Added plugins"
+        cd ../..
+    fi
+
+    # wp-optimize - cache e tabelle, wordpress-seo - seo, better-wp-security antibruteforce, 
+    # elementor pro-elements - plugin del tema, webp-converter-for-media
+    # 
+
 done
